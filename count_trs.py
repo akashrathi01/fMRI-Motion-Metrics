6a4488199e7266519ddfd5ddfb74684d970afe1b
Akash Rathi <144168648+akashrathi01@users.noreply.github.com>
2025-11-23 20:02:50 -0600

    Update count_trs.py

--- count_trs.py
+++ count_trs.py
@@ -4,7 +4,7 @@ import nibabel as nib
 import csv
 
 
-PATH = '/projects/b1108/studies/RADAR/data/processed/neuroimaging/AIB_RestOnly_Networks/*/ses-1/*_final.nii.gz'
+PATH = 'YOUR/PROCESSED/RESTING/NETWORKS'
 
 
 files = glob.glob(PATH)
@@ -13,7 +13,6 @@ tr_counts = [["ID", "ses", "cleaned_shape"]]
 
 
 for file in files:     
-    #sub-t1133_ses-1_task-rest_run-02_fd-1_final.nii.gz
     short_name = os.path.basename(file)
     print(short_name)
     ID = short_name[0:8]
